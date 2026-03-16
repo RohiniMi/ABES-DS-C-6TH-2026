506ba1d53ffe4393670e51a6573ae88b9a0de3d3
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2026-03-16 15:20:37 +0530

    http module

--- day_05/controller/login.js
+++ day_05/controller/login.js
@@ -1,21 +1,21 @@
 import { readFile } from "../utils/readFile.js";
-const FILE = "../users.json";
-const userLogin = async (userDetails) => {
+// const FILE = "../users.json";
+export const userLogin = async (userDetails,FILE) => {
     const { email, password } = userDetails;
 
     const users = await readFile(FILE);
     if (users.length === 0) {
-        console.log("user is not registered.");
-        return;
+        // console.log("user is not registered.");
+        return { status: 400, message: "user is not registered." };
     }
     const user = users.filter((u) => u.email === email);
     if (user.length === 0) {
         console.log("user is not registered.");
-        return;
+        return { status: 400, message: "user is not registered." };
     }
-    user[0].password === password ? console.log("Login Successful") :
-        console.log("Incorrect password");
+    return user[0].password === password ? { status: 200, message: "Login Successfull" } :
+        { status: 500, message: "Incorrect password" };
 }
-userLogin({ email: "dmilmoe0@addtoany.com", password: "tU4.g@/_nl71i#)" });
-userLogin({ email: "dmilmoe0@addtoany.com", password: "tU4.g@/_nldfklgne71i#)" });
-userLogin({ email: "dmilmoe0@addtodjhgany.com", password: "tU4.g@/_nl71i#)" });
+// userLogin({ email: "dmilmoe0@addtoany.com", password: "tU4.g@/_nl71i#)" });
+// userLogin({ email: "dmilmoe0@addtoany.com", password: "tU4.g@/_nldfklgne71i#)" });
+// userLogin({ email: "dmilmoe0@addtodjhgany.com", password: "tU4.g@/_nl71i#)" });
--- day_05/controller/register.js
+++ day_05/controller/register.js
@@ -1,32 +1,33 @@
 import { readFile } from "../utils/readFile.js";
 import { writeFile } from "../utils/writeFile.js";
-const FILE = "../users.json";
+// const FILE = "../users.json";
 
-const register = async (users, userDetails) => {
+const register = async (users, userDetails, FILE) => {
     const updatedData = [...users, userDetails];
     const response = await writeFile(FILE, updatedData);
-    console.log(response.message);
-    return;
+    return response;
 }
-const userRegistration = async (userDetails) => {
+export const userRegistration = async (userDetails, FILE) => {
     const { name, email, password, gender } = userDetails;
 
     if (!name || !email || !password || !gender) {
-        console.log("All field are required.");
-        return;
+        // console.log("All field are required.");
+        return { status: 400, message: "All field are required." };
     }
 
     const users = await readFile(FILE);
     if (users.length === 0) {
-        register(users, userDetails);
+        const response = await register(users, userDetails, FILE);
+        return response;
     }
 
     const existingUser = users.find((u) => u.email === email);
     if (existingUser) {
-        console.log("user is already registered");
-        return;
+        // console.log("user is already registered");
+        return { status: 400, message: "user is already registered" };
     }
-    register(users, userDetails);
+    const response = await register(users, userDetails, FILE);
+    return response;
 }
 userRegistration({
     "email": "rohannen9@dfgnnbkjalbum.net",
